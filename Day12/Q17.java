803f5371c2dd9dca3be491b549ea59fb2d284395
rlaehdwozld <rlaehdwozld@naver.com>
2022-09-06 20:12:14 +0900

    1

---
+++ Day12/Q17.java
@@ -0,0 +1,21 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Q17 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	
+		
+		int[] arr = new int[5];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+			System.out.print(arr[i] + " ");
+
+		}
+
+	}
+}
---
+++ Day12/Q18.java
@@ -0,0 +1,16 @@
+package Day4;
+
+import java.util.Scanner;
+
+public class Q18 {public static void main(String[] args) {
+	
+	Scanner sc= new Scanner(System.in);
+			
+			char []arr=new char[10];
+	
+for(int i=0;i<arr.length;i++) {
+	arr[i]=sc.next().charAt(0);
+	System.out.print(arr[i]);
+}
+}
+}
